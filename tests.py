37d2cba4fa7de2ff5ac38325934160ed98d1ec58
Sp2ceM0nk <bartsimpsom300@gmail.com>
2023-08-23 21:39:18 -0300

    Add requirements

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
-from IPython.display import display
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Links: https://www.youtube.com/watch?v=w5JhFN0CwOE
 #        https://acervolima.com/o-que-o-atributo-tearoff-faz-em-um-menu-tkinter/
@@ -13,16 +14,10 @@ from IPython.display import display
 file = Path(__file__).parent.joinpath("data").joinpath("cargas2011a2021.xlsx")
 cargas_portos = pd.read_excel(file)
 
-years = cargas_portos["Ano"].drop_duplicates().to_list()
+# years = cargas_portos["Ano"].drop_duplicates().to_list()
 
-# years = [2011]
+years = [2011]
 
 harbors = ["Guaruja"]
 
-print(type(years[0]))
 
-# cargas_portos_year = cargas_portos.loc[(cargas_portos["Ano"].isin(years))]
-
-# cargas_portos_year = cargas_portos_year.loc[(cargas_portos["Porto"].isin(harbors))]
-
-# cargas_portos_filtered = cargas_portos_year.to_numpy().tolist()
